b2a04058b8a05dcb21e733e26405b5ee2277f33c
Brian Feaster <brianf@gmail.com>
2015-07-31 21:47:37 +0000

    Beta test version.

--- main/main.go
+++ main/main.go
@@ -2,8 +2,44 @@ package main
 
 import (
 	"fmt"
+	"errors"
+	"encoding/csv"
 )
 
+
+type SimpleFile struct {
+  data string;
+     i int;
+}
+
+func SimpleFileNew (d string) *SimpleFile {
+	return &SimpleFile{data:d, i:0}
+}
+
+func (this *SimpleFile) Read(p []byte) (n int, err error) {
+	if len(this.data) <= this.i {
+		n = 0;
+		err = errors.New("No more data!")
+	} else {
+		p[0] = this.data[this.i]
+		this.i++
+		n = 1
+		err = nil
+	}
+	return
+}
+
 func main () {
-	fmt.Println(".")
+	var strs []string
+	var err error = nil;
+	r := csv.NewReader(SimpleFileNew(
+		"ab,cd,12\n" +
+		"AB,CD,12,34\n" +
+		"x,y,z\n"))
+	for {
+		strs, err = r.Read()
+		fmt.Printf("Output:%v\n", strs)
+		if err!=nil { break }
+	}
+	fmt.Print(err)
 }
