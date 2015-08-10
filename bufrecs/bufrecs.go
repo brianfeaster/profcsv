0f214abbac8cc2d50644cd876657c4ef6ccf710e
Brian Feaster <brianf@gmail.com>
2015-08-10 00:56:25 +0000

    Missed files.

--- bufrecs/bufrecs.go
+++ bufrecs/bufrecs.go
@@ -1,152 +1,143 @@
-package bytequeue
+package bufrecs
 
 import (
+	"bytes"
 	"fmt"
-	"net"
+	"io"
 )
 
-/* Byte stream which derives bytes from an internet connection
-   .Get        get next char/byte from stream
-   .Unget      unget the last char/byte got (one one deep)
-   .Token      get back accumulated character string
-   .TokenClear clear the accumulated char string
+/* Byte slice queue which derives new slices from a queue of slices and a delimeter.
+   .Next   let next complete record (array of byte slices) including the last delimeter.  If no delimeter, it is incomplete and no more records will be produced from the Reader.
 */
 
-type ByteQueue struct {
-	Conn net.Conn // internet connection
-
-	buff         []byte      // Array which buffers all new incoming bytes a slice at a time
-	slices       chan []byte // Channel of new incoming byte slices
-	current      []byte      // Current dequeued slice bytes are pulled from for the user
-	idx          int         // Index into the current dequeued slice
-	undo         bool        // The last character read will be read again
-	recordReset  bool        // Start recording on next get()
-	sliceIdx     int         // Index of current slice to begin recording
-	recordSlices [][]byte    // Array of recorded slices
+type BufRecs struct {
+	reader   io.Reader  // where bytes will come from
+	finalErr error
+	delim    byte       // The delimeter.  Default is ','
+	buff     []byte    // Byte array where all new incoming bytes are writen to and then sliced up.  After filling and sliced, a new stage is allocated.
+	recStage [][]byte // The bytes stage is sliced and queued here until a delimeter is scanned.
+	recQueue chan [][]byte // Channel of records (arrays of byte slices)
+	FinalPartial [][]byte
+	recOut   [][]byte // Dequeued records that are bled out to Read requests
+	err      error
 }
 
-const BufferSize int = 64
-const QueueMax int = 8
+const ByteStageSize int = 8192
+const QueueMax int = 1024
 
 // Initializiation //
 
-func ConnQueueNew() *ByteQueue {
-	conn, err := connQueueNewSetupInterweb()
-	if nil != err {
-		return nil
+func NewBufRecs(r io.Reader) *BufRecs {
+	this := &BufRecs{
+		reader:   r,
+		finalErr: nil,
+		delim:    '\n',
+		buff:     buffMake(),
+		recStage: make([][]byte, 0, 16), // Byte slices are queued here
+		recQueue: make(chan [][]byte, QueueMax), // Complete records (array of byte arrays) are queued up here
+		FinalPartial: nil, // The last partial record, if it exists
+		recOut:   nil,
+		err:      nil,
 	}
-
-	this := &ByteQueue{
-		Conn:         conn,
-		buff:         buffMake(),
-		slices:       make(chan []byte, QueueMax),
-		current:      []byte("."),
-		idx:          0,
-		undo:         false,
-		recordReset:  true,
-		sliceIdx:     0,
-		recordSlices: [][]byte(nil),
-	}
-
-	connQueueNewLaunchDaemons(this)
-
+	go this.readBytesLoop() // Start byte reader thread
 	return this
-}
-
-func connQueueNewSetupInterweb() (conn net.Conn, err error) {
-	conn, err = net.Dial("tcp", "72.14.188.107:7155")
-	return
-}
-
-func connQueueNewLaunchDaemons(this *ByteQueue) {
-	go this._readBytesLoop() // Start internet reader thread
 }
 
 // Helpers //
 
+/* Create a new byte buffer where incoming bytes are spliced from.
+ */
 func buffMake() (buff []byte) {
-	buff = make([]byte, BufferSize)
-	for i, _ := range buff {
-		buff[i] = '.'
-	}
+	buff = make([]byte, ByteStageSize)
 	return
 }
 
 // Methods Private //
 
-/* Always read bytes from the internet and keep track in a channel of byte slices
+/* Always read bytes from the Reader into rope
  */
-func (this *ByteQueue) _readBytesLoop() {
-	//orig := this.buff // Keep track of entire buffer for debugging
+func (this *BufRecs) readBytesLoop() {
 	for {
-		n, err := this.Conn.Read(this.buff)
-		if nil != err {
-			fmt.Println("Error: _readBytesLoop can't Read().")
-			return
-		}
-		this.slices <- this.buff[:n] // Stash read slice into channel
-		//fmt.Printf("\n[%s]", orig)   // Debug the dump the entire full buffer
+		// Snarf some bytes into our local buffer (which is shifted until full)
+		n, err := this.reader.Read(this.buff)
 
-		// Create a new buffer if it's full.  Otherwise adjust the remaining buffer slice.
-		if n == len(this.buff) {
-			this.buff = buffMake()
-			//orig = this.buff
-		} else {
-			this.buff = this.buff[n:]
+		if n <= 0 { fmt.Println("WARNING: readyBytesLoop got nothing.  Expected synchronous behavior.") }
+
+		for 0 < n { // Over all bytes...
+			di := bytes.IndexByte(this.buff[:n], this.delim) // Find a delimiter
+			if di < 0 { // No delimeter [.....n   ]...
+				this.recStage = append(this.recStage, this.buff[:n]) // Move slice to stage
+				this.buff = this.buff[n:] // Shrink buffer to remaining empty bytes
+				break
+			}
+			// A delimeter found [..d...n   ]
+			this.recStage = append(this.recStage, this.buff[:di+1]) // Move slice to stage
+
+			// Add the staged record to the channel for human consumption
+			this.recQueue <- this.recStage
+			this.recStage = make([][]byte, 0, 16)
+
+			this.buff = this.buff[di+1:] // Shrink buffer to remaining empty bytes
+			n = n - (di + 1) // Reduce number of unscanned bytes
 		}
-	}
+
+		// If the buffer has been used up, create a new one
+		if 0 == len(this.buff) {
+			this.buff = buffMake()
+		}
+
+		if err != nil {
+			if 0 < len(this.recStage) {
+				this.FinalPartial = this.recStage
+			}
+			this.finalErr = err
+			break
+		}
+	} // for
+	this.recQueue <- nil // Add a sentinel value to the queue
 }
 
 // Methods Public //
 
-/* Read a byte from the stream.
+
+/* Read a record from the stream
  */
-func (this *ByteQueue) Get() byte {
-	if this.undo { // Return last read character
-		this.undo = false
-	} else if this.idx == (len(this.current) - 1) { // Consider new slice of bytes
-		if !this.recordReset { // If recording, stash this slice
-			this.recordSlices = append(this.recordSlices, this.current[this.sliceIdx:])
+func (this *BufRecs) Get() []byte {
+	rec := <- this.recQueue
+	if nil == rec { return nil }
+	a := rec[0]
+	for _, b := range rec[1:] { a = append(a, b...) }
+	return a
+}
+
+
+func (this *BufRecs) Read(p []byte) (n int, err error) {
+
+	if nil == this.recQueue {
+		err = this.finalErr
+		return
+	}
+
+	for {
+		if nil == this.recOut || 0==len(this.recOut){
+			this.recOut = <- this.recQueue
+			if nil == this.recOut {
+				this.recQueue = nil
+				err = this.finalErr
+				return
+			}
 		}
-		this.current = <-this.slices // Consider next slice of unread bytes
-		this.idx = 0
-		this.sliceIdx = 0 // Reset token index.  Continue recording from beginning of this slice.
-	} else {
-		this.idx++
-	}
 
-	if this.recordReset {
-		this.recordReset = false
-		this.sliceIdx = this.idx // Start recording current slice's bytes from this index
-		this.recordSlices = this.recordSlices[0:0]
-	}
-
-	return this.current[this.idx] // Here's your byte.  All that for one little byte.
-}
-
-func (this *ByteQueue) Token() string {
-	var buf []byte
-	var offset int = 1
-	if this.undo {
-		offset = 0
-	}
-	if !this.recordReset {
-		for _, s := range this.recordSlices {
-			buf = append(buf, s...)
+		// Copy as much of the first record's slice as we can
+		s := copy(p[n:], this.recOut[0])
+		n = n + s
+		if s == len(this.recOut[0]) {  // If all of the slice was copied, remove it, otherwise shift it
+			this.recOut = this.recOut[1:]
+		} else {
+			this.recOut[0] = this.recOut[0][s:]
 		}
-		buf = append(buf, this.current[this.sliceIdx:this.idx+offset]...)
+		if 0==s || n==len(p) { break } // If nothing copied or p full, return
 	}
-	return string(buf)
-}
 
-/* /Reset the recording of bytes that represent the current scanned token
- */
-func (this *ByteQueue) TokenClear() {
-	this.recordReset = true
-}
-
-/* Undo the last byte read from the stream.
- */
-func (this *ByteQueue) Unget() {
-	this.undo = true
+	return
 }
--- main/main.go
+++ main/main.go
@@ -1,31 +1,147 @@
 package main
 
 import (
+	"github.com/brianfeaster/profcsv/bufrecs"
+	"bufio"
 	"bytes"
 	"encoding/csv"
-	"runtime"
 	"fmt"
- "io"
+	"io"
 	"net/http"
 	"net/url"
-	"bufio"
 	"os"
+	"runtime"
 	"strconv"
 	"strings"
 	"sync"
 	"time"
+	"errors"
 )
+
+// Helper s
+
+func urlContentLength(url string) (len int64) {
+	urlHeader, e := http.Head(url)
+	if e != nil {
+		len = -1.0
+	} else {
+		l, _ := strconv.ParseInt(urlHeader.Header["Content-Length"][0], 10, 64)
+		len = int64(l)
+	}
+	return
+}
+
+func min (a, b int) int {
+	if a < b { return a } else { return b }
+}
+
+func stringMin (a []byte) string {
+	var l int = min(40, len(a))
+	return string(a[0:l]) + " ... " + string(a[len(a)-l:len(a)])
+}
+
+/* NewLineReader -- Implements a ReaderCloser that will only return characters that end in newline.  It will keep track of initial characters (which can't be determined if they have been preceeded by a newline) and trailing characters that are not delmited by a newline.
+*/
+const NewlineReaderBufferSize = 4096
+type NewlineReader struct {
+	reader io.ReadCloser
+	buffSize int
+	pre    []byte // inital bytes plus newline   [------/] or [-------]
+	post   []byte // final bytes (no newline)    [-------]
+	tosend []byte
+}
+
+func NewNewlineReader (reader io.ReadCloser) *NewlineReader{
+	return &NewlineReader{
+		reader:reader,
+		pre:make([]byte, 0, NewlineReaderBufferSize),
+		post:make([]byte, 0, NewlineReaderBufferSize),
+		tosend:make([]byte, 0, NewlineReaderBufferSize)}
+}
+
+/* Need to accumulate records in a queue
+
+	If pre empty:
+		Read until a newline is seen.  Copy everythying to pre
+*/
+
+func (this *NewlineReader) Read (b []byte) (n int, err error) {
+
+	// If anything to send, sent it out.
+	if 0 < len(this.tosend) {
+fmt.Println("sending only...")
+fmt.Print(" SEND:", stringMin(this.tosend), "\n\n")
+		n = copy(b, this.tosend)
+		err = nil
+fmt.Println("sent ", n)
+		this.tosend = this.tosend[n:len(this.tosend)]
+fmt.Print(" SEND:", stringMin(this.tosend), "\n\n")
+		return
+	}
+
+
+	if 0==len(this.pre) || '\n' != this.pre[len(this.pre)-1] { // Haven't found a newline yet.  Read more into it
+
+fmt.Print("pre not full...")
+		l, e := this.reader.Read(b) // Use buffer passed in (will have a length, not sure of the capacity)
+
+		if l<=0 || e!=nil { // If unable to read anything, yield
+			n = l
+			err = e
+fmt.Println("returning")
+			return
+		}
+
+		offset := bytes.IndexByte(b[:l], '\n') // Look for a newline
+		if (0 <= offset) { // Found a newline so keep track in pre and the rest goes in post
+fmt.Print("found newline at", offset)
+			this.pre = append(this.pre, b[:offset+1]...)
+			this.post = append(this.post, b[offset+1:l]...)
+		} else {           // No newline, so just continue copying
+			this.pre = append(this.pre, b...)
+		}
+if 0!=len(this.pre) { fmt.Printf("last pre char is %d", this.pre[len(this.pre)-1]) }
+fmt.Print("\n  PRE:", stringMin(this.pre))
+fmt.Print(" POST:", stringMin(this.post), "\n")
+fmt.Print(" SEND:", stringMin(this.tosend), "\n\n")
+	} else { // Read more bytes and stash into post
+fmt.Print("pre is full...")
+		l, e := this.reader.Read(b)
+fmt.Println("\nreader.Read ", string(b))
+		if l<=0 || e!=nil { // If unable to read anything, yield
+			n = l
+			err = e
+			return
+		}
+		offset := bytes.IndexByte(b[:l], '\n') // Look for a newline
+		if (0 <= offset) { // Found a newline so move post and this to tosend
+			this.tosend = this.post
+			this.tosend = append(this.tosend, b[:offset+1]...)
+			this.post = b[offset+1:]
+		} else {           // No newline, so just continue copying
+			this.post = append(this.post, b...)
+		}
+fmt.Print("\n  PRE:", stringMin(this.pre))
+fmt.Print(" POST:", stringMin(this.post), "\n")
+fmt.Print(" SEND:", stringMin(this.tosend), "\n\n")
+	}
+
+	return
+}
+
+
+// Field counters
 
 const FieldMax int = 500
 
-func countFields (reader *csv.Reader, header string) {
+func countFields(reader *csv.Reader, header string) {
 	var f int
 	var counts [FieldMax]int
 	var strs []string
 	var err error
 	reader.FieldsPerRecord = -1
 
-	var t0 time.Time = time.Now()
+	var startTime time.Time = time.Now()
 
 	err = nil
 	for {
@@ -36,30 +152,6 @@ func countFields (reader *csv.Reader, header string) {
 		} else {
 			fmt.Println("ERROR: more than", FieldMax, "fields")
 		}
-		if err != nil { break }
-	}
-	fmt.Print(header)
-	for f=0; f<FieldMax; f++ {
-		if 0 < counts[f] { fmt.Println(f, counts[f]) }
-	}
-
-	var t1 time.Time = time.Now()
-
-	fmt.Printf("The call took %f seconds.\n\n", t1.Sub(t0).Seconds())
-}
-
-func countFieldsNEW(reader *csv.Reader, header string) {
-	var f int
-	var counts [FieldMax]int
-	//var strs []string
-	var err error
-	reader.FieldsPerRecord = 227
-
-	var t0 time.Time = time.Now()
-
-	err = nil
-	for {
-		_, err = reader.Read()
 		if err != nil {
 			break
 		}
@@ -67,66 +159,26 @@ func countFieldsNEW(reader *csv.Reader, header string) {
 	fmt.Print(header)
 	for f = 0; f < FieldMax; f++ {
 		if 0 < counts[f] {
-			fmt.Println(f, counts[f]) } }
-
-	var t1 time.Time = time.Now()
-
-	fmt.Printf("The call took %f seconds.\n\n", t1.Sub(t0).Seconds())
-}
-
-func worker(wg *sync.WaitGroup, purl *url.URL, i int64, rng string) {
-	defer wg.Done()
-	filename := fmt.Sprintf("data-%015d", i)
-	fmt.Printf("%s: getting range %s\n", filename, rng)
-	c := &http.Client{}
-	resp, err := c.Do(&http.Request{URL: purl, Header: http.Header{"Range": []string{rng}}})
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != 206 {
-		panic(fmt.Errorf("non 206: %s\n", resp))
-	}
-	serversays := strings.Split(strings.Split(resp.Header["Content-Range"][0], "/")[0], "-")
-	serversays[0] = strings.TrimPrefix(serversays[0], "bytes ")
-	if len(serversays) == 2 && serversays[0] == serversays[1] {
-		fmt.Printf("%s: empty\n", filename)
-		return
-	}
-	now := time.Now()
-	o, err := os.Create(filename)
-	defer o.Close()
-	if err != nil {
-		panic(err)
-	}
-	numbytes, err := io.Copy(o, resp.Body)
-	csvreader := csv.NewReader(bufio.NewReaderSize(resp.Body, 32767))
-	csvreader.FieldsPerRecord = -1
-	for {
-		_, err := csvreader.Read()
-		if err != nil {
-			break
+			fmt.Println(f, counts[f])
 		}
 	}
-	if err != nil { panic(err) }
-	fmt.Printf("\"%s\"  bytes:%d  duration:%s  len:%s  content-range:%s\n", filename, numbytes, time.Since(now), resp.Header["Content-Length"][0], resp.Header["Content-Range"][0])
-}
 
+	fmt.Println("Done.", time.Since(startTime))
+}
 
 
 /* Exercises the countFields function on a simple string derived CSV.
-*/
-func testReadString () {
+ */
+func testReadString(str []byte, desc string) {
 	var reader *csv.Reader
-
-	reader = csv.NewReader(bytes.NewReader([]byte("ab,cd,12\nAB,CD,12,34\nx,y,z\n")))
-	countFields(reader, "static string")
+	reader = csv.NewReader(bytes.NewReader(str))
+	countFields(reader, desc)
 }
 
 
 /* Exercises the countFields function on a local CSV file.
-*/
-func testReadFile () {
+ */
+func testReadFile() {
 	var reader *csv.Reader
 	var err error
 	var f *os.File
@@ -148,47 +200,146 @@ func testReadFile () {
 	countFields(reader, "file")
 }
 
+// Concurrent http file read ////////
 
-func testSplit() {
-	threadCount := runtime.NumCPU()
-	runtime.GOMAXPROCS(threadCount)
-	var err error
-	u := "http://west.xfltr.com.s3-us-west-2.amazonaws.com/census/ss10pusa.csv"
-	urlParsed, err := url.Parse(u)
-//fmt.Println("::urlParsed", urlParsed)
-	if err != nil {
-		panic(err)
-	}
-	urlHeader, err := http.Head(u)
-	if err != nil {
-		panic(err)
-	}
-//fmt.Println("::urlHeader", urlHeader)
-//for a, b := range(urlHeader.Header) { fmt.Println("::urlHeader", a, b) }
-	leng, err := strconv.ParseInt(urlHeader.Header["Content-Length"][0], 10, 64)
-	fmt.Printf("length = %d\n", leng)
-	sz := leng / int64(threadCount)
-	wg := sync.WaitGroup{}
-	started := time.Now()
-	for i := int64(0); i < leng; i += sz {
-		upper := i + sz -1
-		done := false
-		if upper > leng {
-			upper = leng
-			done = true
-		}
-		rng := fmt.Sprintf("bytes=%d-%d", i, upper)
-		wg.Add(1)
-		go worker(&wg, urlParsed, i, rng)
-		if done { break }
-	}
-	fmt.Printf("waiting...\n")
-	wg.Wait()
-	fmt.Printf("took %s\n", time.Since(started))
+var Sources = []string{
+	"http://west.xfltr.com.s3-us-west-2.amazonaws.com/census/ss10pusa.csv", // 3.502 Gb
+	"http://west.xfltr.com.s3-us-west-2.amazonaws.com/census/ss10pusd.csv", // 2.941 Gb
+	"http://west.xfltr.com.s3-us-west-2.amazonaws.com/census/ss10pusc.csv", // 2.806 Gb
+	"http://west.xfltr.com.s3-us-west-2.amazonaws.com/census/ss10pusb.csv"} // 2.643 Gb
+
+func recordFlatten (rec [][]byte) (f []byte) {
+	f = make([]byte, 0)
+	for _, s := range rec { f = append(f, s...) }
+	return
 }
 
+var Partials [][]byte
+
+func testSplitWorker(wg *sync.WaitGroup, urlParsed *url.URL, threadId int64, from int64, to int64) {
+	defer wg.Done()
+
+	now := time.Now()
+
+	// Prep http request header field
+
+	rng := fmt.Sprintf("bytes=%d-%d", from, (to - 1)) // The http range header which specifies an inclusive range.
+
+	fmt.Printf("%d:: %s %d\n", threadId, rng, to-from)
+
+	// Open connection
+
+	client := &http.Client{}
+	resp, err := client.Do(&http.Request{URL: urlParsed, Header: http.Header{"Range": []string{rng}}})
+	if err != nil {
+		panic(err)
+	}
+
+	// Consider response
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 206 {
+		panic(fmt.Errorf("%d:: non 206: %s\n", threadId, resp))
+	}
+
+	serversays := strings.Split(strings.Split(resp.Header["Content-Range"][0], "/")[0], "-")
+
+	fmt.Printf("%d:: serversays:%s\n", threadId, serversays)
+
+	serversays[0] = strings.TrimPrefix(serversays[0], "bytes ")
+
+	if len(serversays) == 2 && serversays[0] == serversays[1] {
+		fmt.Printf("%d:: server reports empty\n", threadId)
+		return
+	}
+
+	filename := fmt.Sprintf("data-%02d", threadId)
+	o, err := os.Create(filename)
+	defer o.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	var copiedBytes int64 = 0
+	//copiedBytes, err = io.Copy(o, resp.Body)
+
+	//csvreader := csv.NewReader(resp.Body)
+	//csvreader := csv.NewReader(bufio.NewReader(resp.Body))
+	var bufr *bufrecs.BufRecs = bufrecs.NewBufRecs(resp.Body)
+	Partials[threadId*2] = bufr.Get()
+	csvreader := csv.NewReader(bufr) // Pass the remote file chunk to csv
+
+	csvreader.FieldsPerRecord = -1
+	countFields(csvreader, fmt.Sprintf("http_%d", threadId))
+
+	Partials[threadId*2+1] = recordFlatten(bufr.FinalPartial)
+
+	fmt.Printf("%d:: \"%s\"  bytes:%d  duration:%s  len:%s  content-range:%s\n", threadId, filename, copiedBytes, time.Since(now), resp.Header["Content-Length"][0], resp.Header["Content-Range"][0])
+}
+
+func testSplit() {
+	var e error
+	var startTime time.Time = time.Now()
+	var threadCount int64 = int64(runtime.NumCPU()) * 2
+	var uri string = Sources[3]
+	var urlLength, t, from, to, checkCount int64
+	var done bool
+
+	runtime.GOMAXPROCS(int(threadCount))
+
+	Partials = make([][]byte, threadCount*2)
+
+	fmt.Printf("URL\t\"%s\"\n", uri)
+
+	urlLength = urlContentLength(uri)
+	if urlLength < 0 {
+		panic(e)
+	}
+
+	splitSize := (urlLength / threadCount)
+
+	fmt.Printf("Content len\t[%d %0.3f Gb]\n", urlLength, float64(urlLength)/(1024.0*1024.0*1024.0))
+	fmt.Printf("Split size\t[%d]\n", splitSize)
+
+	waitGroup := sync.WaitGroup{}
+
+	urlParsed, e := url.Parse(uri)
+	if e != nil {
+		panic(e)
+	}
+
+	for t, from, done = 0, 0, false; (!done); t, from =t+1, to {
+		to = from + splitSize
+		if (t == (threadCount - 1)) || (to >= urlLength) {
+			to = urlLength
+			done = true
+		}
+		waitGroup.Add(1)
+		go testSplitWorker(&waitGroup, urlParsed, t, from, to)
+		checkCount += (to - from)
+	}
+
+	if checkCount != urlLength {
+		panic(errors.New("ERROR!  Sum of separate content-lengths do not equal the original URL's content length"))
+	}
+
+	fmt.Println("Waiting on", t, "threads...")
+	waitGroup.Wait()
+
+	for _, r := range Partials {
+		fmt.Println("Partials:", string(r))
+	}
+
+	testReadString(recordFlatten(Partials), "Reassembled interchunk partials.")
+
+	fmt.Println("Done.", time.Since(startTime))
+} // testsplit
+
+// Main ////////
+
 func main() {
-	//testReadString()
+	//testReadString([]byte("ab,cd,12\nAB,CD,12,34\nx,y,z\n", "static string"))
 	//testReadFile()
-	testSplit();
+	testSplit()
 }
